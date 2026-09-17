9bd07780a3de34dd3139b72524046e7bdadf6847
Anshul227 <anshulmoon227@gmail.com>
2026-09-17 21:20:22 +0530

    Initial commit

---
+++ jdbccrud/Authentication.java
@@ -0,0 +1,79 @@
+package jdbccrud;
+import java.sql.*;
+public class Authentication {
+
+	static String table = " from crud.users ";
+	
+	
+	public static boolean passCheck(String userId,String pass,Connection conn) {
+		
+		userId = "'"+userId+"'";
+		
+		try {
+			PreparedStatement stm = conn.prepareStatement("select password"+table+"where user_id = "+userId);
+			ResultSet rs = stm.executeQuery();
+			if(!rs.next()) {
+				return false;
+			}
+			//System.out.println(rs.getString("password"));
+			
+			String userPass = rs.getString("password");
+			if(decrypt(userPass).equals(pass)) {
+				return true;
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	
+	
+	
+	public static String encrypt(String pass) {
+		int key = 2;
+		char[] arr = pass.toCharArray();
+		
+		String encPass = "";
+		
+		for(char ch : arr) {
+			encPass = encPass + (char)((int)ch+key);
+		}
+		
+		return encPass;
+		
+	}
+	
+	public static String decrypt(String pass) {
+		
+		int key = 2;
+		char[] arr = pass.toCharArray();
+		
+		String dePass = "";
+		
+		for(char ch : arr) {
+			dePass = dePass + (char)((int)ch-key);
+		}
+		
+		return dePass;
+	}
+	
+	public static boolean exists(String userId,Connection conn) {
+//		conn = null;
+		userId = "'"+userId+"'";
+		try {
+			PreparedStatement stm = conn.prepareStatement("select id "+table+"where user_id = "+userId );
+			ResultSet rs = stm.executeQuery();
+			if (!rs.next()) {
+				return false;
+			}
+			
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return true;
+		
+	}
+}
